d06ea963ad62541d56ea6fe5aa6b7a6c3f17fab9
Luciano Galhardo <lrgalhardo@hotmail.com>
2025-07-10 09:54:33 -0300

    Estrutura inicial automação login

--- automacao_target/cypress/e2e/login.cy.js
+++ automacao_target/cypress/e2e/login.cy.js
@@ -1,11 +1,18 @@
 describe('Login', () => {
     it('realizar login com sucesso', () => {
         // Arrange
+        Cypress.on('uncaught:exception', (err, runnable) => {
+          // Verifica se o erro é o que você quer ignorar
+            if (err.message.includes("themeDisplay.getUser is not a function")) {
+                return false; // impede o Cypress de falhar o teste
+                }
+            });
+
         cy.visit('https://fapsoftex.plataformatarget.com.br/web/fap/login'); 
         // Act
-        cy.get('_58_login').type('lrgalhardo@hotmail.com')
+        cy.get('#_58_login').type('lrgalhardo@hotmail.com')
 
-        cy.get('_58_password').type('faptarget123')
+        cy.get('#_58_password').type('faptarget123')
 
         cy.get('.tg-form-login > .buttons > .btn').click()
 
